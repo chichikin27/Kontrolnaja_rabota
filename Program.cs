70829db215df7d77530ebfa4cba7cb8773b4798a
Victor <chichikin27@mail.ru>
2023-02-27 18:53:37 +0300

    Хотел списать, но здравый смысл победил! Создали массив с циклом ввода с клавиатуры

--- Program.cs
+++ Program.cs
@@ -1,24 +1,11 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+﻿
+
+Console.Write("Введитие колиество элементов: ");
+int size = int.Parse(Console.ReadLine()!);
+string[] array = new string[size];
+
+for(int i = 0; i < size; i++)
 {
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    array[i] = Console.ReadLine();
 }
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
