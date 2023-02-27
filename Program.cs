109bb298b81783f4638ad7ed9840800901a184c8
Victor <chichikin27@mail.ru>
2023-02-27 19:08:01 +0300

    Доделал вывод массива

--- Program.cs
+++ Program.cs
@@ -4,8 +4,13 @@ Console.Write("Введитие колиество элементов: ");
 int size = int.Parse(Console.ReadLine()!);
 string[] array = new string[size];
 
-for(int i = 0; i < size; i++)
+for(int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите элемент массива под индексом {i}: ");
     array[i] = Console.ReadLine();
+}
+Console.WriteLine("Вы ввели следующие строки: ");
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
 }
