26c18fc93dfdac2d0b9fad5dce3a49855e1a6b46
Victor <chichikin27@mail.ru>
2023-02-27 20:01:16 +0300

    Доделала вывод, если таких элементов нету

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
 ﻿
-
 Console.Write("Введитие колиество элементов: ");
 int size = int.Parse(Console.ReadLine()!);
 string[] array = new string[size];
-
+int count = size;
+Console.WriteLine("===========================================");
 for(int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите элемент массива под индексом {i}: ");
     array[i] = Console.ReadLine();
 }
-Console.WriteLine("Вы ввели следующие строки: ");
+Console.WriteLine("===========================================");
+Console.WriteLine("Вы ввели следующие элементы: ");
 for(int i = 0; i < array.Length; i++)
 {
     Console.WriteLine(array[i]);
+}
+Console.WriteLine("===========================================");
+Console.WriteLine("Элементы содержащие три и меньше символов: ");
+for(int i = 0; i < array.Length; i++)
+
+{
+    if(array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+        count = count - 1;
+    }
+    else if(count == size)
+    {
+        Console.WriteLine("Таких элементов нету");
+        break;
+    }
+
 }
